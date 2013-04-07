bfc50adf3e390d9b7465721f50cf168565f9bbae
Alex Edwards <ajmedwards@gmail.com>
2013-04-07 14:26:12 +0100

    add: documentation for public methods

--- lib/listless.rb
+++ lib/listless.rb
@@ -4,6 +4,14 @@ require "htmlentities"
 module Listless
   extend self
 
+  # Create a HTML unordered list from an array
+  #
+  # @param array [Array]
+  # @return [String]
+  #
+  # @example
+  #   Listless.ul ['foo', 'bar'] #=> "<ul><li>foo</li><li>bar</li></ul>"
+
   def ul(array)
     list = array.map { |x| tag(:li, escape(x)) }.join
     tag(:ul, list)
